544d94f52996b5bbb1916d2a53f56d80121d864b
Sukh Arora <sarora2073@gmail.com>
2019-01-11 08:40:32 -0500

    cc

--- src/app.js
+++ src/app.js
@@ -1,31 +1,37 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import { createDropdown, addListToDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import {
+	createDropdown,
+	addListToDropdown
+} from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
 
 export default class MergeTags extends Plugin {
 	init() {
 
 
-        const editor = this.editor;
-        const t = editor.t;
-        const defaultTitle = t('Add image');
-        const dropdownTooltip = t('Image');
+		const editor = this.editor;
+		const t = editor.t;
+		const defaultTitle = t('Add image');
+		const dropdownTooltip = t('Image');
 
-        // Register UI component
-        editor.ui.componentFactory.add('mergeTags', locale => {
+		// Register UI component
+		editor.ui.componentFactory.add('mergeTags', locale => {
 
-            const dropdownView = createDropdown( locale );
+			const dropdownView = createDropdown(locale);
 
-            dropdownView.set({
-                label: 'Image',
-                tooltip: true
-            });
-            dropdownView.buttonView.set( {
+			dropdownView.set({
+				label: 'Image',
+				tooltip: true
+			});
+			dropdownView.buttonView.set({
 				isOn: false,
 				withText: true,
 				tooltip: dropdownTooltip
-            });
-            dropdownView.extendTemplate( {
+			});
+			dropdownView.extendTemplate({
 				attributes: {
 					class: [
 						'ck-image-dropdown'
@@ -33,55 +39,53 @@ export default class MergeTags extends Plugin {
 				}
 			});
 
-            // The collection of the list items.
-            const items = new Collection();
+			// The collection of the list items.
+			const items = new Collection();
 
-            items.add( {
-                type: 'button',
-                model: new Model( {
-                    label: 'Upload image',
-                    icon: imageIcon
-                })
-            });
+			items.add({
+				type: 'button',
+				model: new Model({
+					label: 'Upload image',
+					icon: imageIcon
+				})
+			});
 
-            items.add( {
-                type: 'button',
-                model: new Model( {
-                    label: 'Image URL',
-                    icon: imageIcon
-                })
-            });
+			// items.add({
+			// 	type: 'button',
+			// 	model: new Model({
+			// 		label: 'Image URL',
+			// 		icon: imageIcon
+			// 	})
+			// });
 
-            // Create a dropdown with a list inside the panel.
-            addListToDropdown( dropdownView, items );
+			// Create a dropdown with a list inside the panel.
+			addListToDropdown(dropdownView, items);
 
-            return dropdownView;
-        });
-    }
-
-		// console.log('MergeTags was initialized');
-		// const editor = this.editor;
-
-		// editor.ui.componentFactory.add('mergeTags', locale => {
-
-		// 	const dropdownView = createDropdown( locale );
-
-		// 	const view = new ButtonView(locale);
-
-		// 	view.set({
-		// 		label: 'Insert image',
-		// 		icon: 'imageIcon',
-		// 		tooltip: true
-		// 	});
-
-		// 	// Callback executed once the image is clicked.
-		// 	view.on('execute', () => {
-		// 		// const imageURL = prompt('Image URL');
-		// 		console.log('clicked');
-		// 	});
-
-		// 	return view;
-		// });
+			return dropdownView;
+		});
 	}
 
+	// console.log('MergeTags was initialized');
+	// const editor = this.editor;
+
+	// editor.ui.componentFactory.add('mergeTags', locale => {
+
+	// 	const dropdownView = createDropdown( locale );
+
+	// 	const view = new ButtonView(locale);
+
+	// 	view.set({
+	// 		label: 'Insert image',
+	// 		icon: 'imageIcon',
+	// 		tooltip: true
+	// 	});
+
+	// 	// Callback executed once the image is clicked.
+	// 	view.on('execute', () => {
+	// 		// const imageURL = prompt('Image URL');
+	// 		console.log('clicked');
+	// 	});
+
+	// 	return view;
+	// });
 }
